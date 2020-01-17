6406b426969ee130964d3f4aef7fb012837cb4c2
Elliot Jordan <ewjintl@icloud.com>
2020-01-17 14:36:01 -0800

    add 'try again' output and accompanying image, tweak jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
   $('form#questions').submit(function(event) {
     var os = $("select#os").val();
-    var side = $("select#class").val();
+    var side = $("select#side").val();
     var application = $("select#application").val();
-    var experience = $("select#experience").val();
-    var stack = $("select#stack").val();
+    // var experience = $("select#experience").val();
+    // var stack = $("select#stack").val();
     // console.log(os + side + application + experience + stack);
 
 
+      if ((os === "1" && application === "3") || (side === "3" && application === "3")) {
+        $("#c").show();
+      } else {
+        $("#again").show();
+      }
 
-  
-  event.preventDefault();
+    event.preventDefault();
+    });
   });
-});
