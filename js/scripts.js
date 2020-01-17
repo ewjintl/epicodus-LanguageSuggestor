bbf3a44535fd7f67e6022f69b0395394bec5dc87
Elliot Jordan <ewjintl@icloud.com>
2020-01-17 15:11:43 -0800

    modify jquery

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,24 @@ $(document).ready(function() {
     var os = $("select#os").val();
     var side = $("select#side").val();
     var application = $("select#application").val();
-    // var experience = $("select#experience").val();
-    // var stack = $("select#stack").val();
-    // console.log(os + side + application + experience + stack);
+    var experience = $("select#experience").val();
+    var stack = $("select#stack").val();
+    console.log(os + side + application + experience + stack);
 
 
       if ((os === "1" && application === "3") || (side === "3" && application === "3")) {
         $("#c").show();
-      } else {
-        $("#again").show();
-      }
+      } else if ((os === "5" && side === "1") || (side === "1" && application === "2")) {
+          $("#js").show();
+        }  else if ((os === "5" && side === "1") || (side === "1" && application === "1")) {
+          $("#css").show();
+        }  else if ((os === "5" && side === "1") || (side === "1" && application === "1")) {
+          
+        
+
+        else {
+            $("#again").show();
+        }
 
     event.preventDefault();
     });
